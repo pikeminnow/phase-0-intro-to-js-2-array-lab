a0fc207bd58fb2430629d0605bf7c8a91979dcb7
Ashton MacKenzie <pikeminnow@gmail.com>
2022-07-20 15:18:29 -0400

    lab activities

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.splice([cats.length],1,name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.splice(0,0,name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.splice([cats.length-1],1);
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.splice(0,1);
+}
+
+function appendCat(name){
+    const allCats = [...cats,name];
+    return allCats;
+}
+
+function prependCat(name){
+    const allCats = [name,...cats];
+    return allCats;
+}
+
+function removeLastCat(){
+    return cats.slice(0,[cats.length-1]);
+}
+
+function removeFirstCat(){
+    return cats.slice(1,[cats.length]);
+}
